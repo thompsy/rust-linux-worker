3672c41b9a8429d2873eaaf745bd674f7ed5ed04
Andrew Thompson <thompsy@downthewire.co.uk>
2021-09-02 21:10:07 +0100

    Add very basic implementation of most worker methods.

--- src/server.rs
+++ src/server.rs
@@ -4,37 +4,67 @@ use tonic::{transport::Server, Request, Response, Status};
 use futures::Stream;
 
 use api::worker_server::{Worker, WorkerServer};
-use api::{Command, Empty, JobId, Log, StatusResponse};
+use api::{JobId, StatusResponse};
+
 use log::LevelFilter;
+use std::collections::HashMap;
+use uuid::Uuid;
+
+use std::sync::Mutex;
 
 pub mod api {
     tonic::include_proto!("api");
 }
 
-#[derive(Debug, Default)]
-pub struct WorkerService {}
+#[derive(Debug)]
+pub struct WorkerService {
+    my_worker: MyWorker,
+}
 
 #[tonic::async_trait]
 impl Worker for WorkerService {
     async fn submit(&self, request: Request<api::Command>) -> Result<Response<JobId>, Status> {
         log::info!("Got a request: {:?}", request.get_ref());
 
-        // let reply = hello_world::HelloReply {
-        //     message: format!("Hello {}!", request.into_inner().name).into(),
-        // };
-        //
-        // Ok(Response::new(reply))
-        Result::Err(Status::unimplemented("submit is not yet implemented"))
+        let result = self.my_worker.submit(request.get_ref().command.to_string());
+        
+        match result {
+            Ok(uuid) => Ok(Response::new(api::JobId { id: uuid.to_string(), })),
+            Err(e) => Err(Status::new(tonic::Code::Internal, e))
+        }
     }
 
     async fn stop(&self, request: tonic::Request<JobId>) -> Result<tonic::Response<api::Empty>, tonic::Status> {
         log::info!("stopping {:?}", request.get_ref());
-        Result::Err(Status::unimplemented("stop is not yet implemented"))
+
+        let parsed = Uuid::parse_str(&request.get_ref().id);
+        if parsed.is_err() {
+            return Err(Status::invalid_argument("invalid UUID"));
+        }
+        let result = self.my_worker.kill(parsed.unwrap());
+        match result {
+            Ok(_) => Ok(Response::new(
+                api::Empty{})),
+            Err(e) => Result::Err(Status::new(tonic::Code::Internal, e))
+        }
     }
 
     async fn status(&self, request: tonic::Request<JobId>, ) -> Result<tonic::Response<StatusResponse>, tonic::Status> {
         log::info!("status {:?}", request.get_ref());
-        Result::Err(Status::unimplemented("status is not yet implemented"))
+
+        let parsed = Uuid::parse_str(&request.get_ref().id);
+        if parsed.is_err() {
+            return Err(Status::invalid_argument("invalid UUID"));
+        }
+        let result = self.my_worker.status(parsed.unwrap());
+        match result {
+            Ok(_) => Ok(Response::new(
+                api::StatusResponse{ 
+                    status: 1,
+                    exit_code: 0,
+            })),
+            Err(e) => Result::Err(Status::new(tonic::Code::Internal, e))
+        }
     }
 
     type GetLogsStream = Pin<Box<dyn Stream<Item = Result<api::Log, Status>> + Send + Sync + 'static>>;
@@ -55,7 +85,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // for some reason 127.0.0.1 didn't work here
     let addr = "0.0.0.0:50051".parse()?;
-    let worker = WorkerService::default();
+    let worker = WorkerService {
+        my_worker: MyWorker::new(),
+    };
 
     Server::builder()
         .add_service(WorkerServer::new(worker))
@@ -63,4 +95,52 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .await?;
 
     Ok(())
+}
+
+
+
+//TODO: rename this to something better. Or maybe rename the other Worker.
+#[derive(Debug)]
+struct MyWorker {
+    //TODO: this should be a sensible struct rather than a string in due course
+    jobs: Mutex<HashMap<Uuid, String>>,
+}
+
+impl MyWorker {
+
+    fn new() -> MyWorker {
+        MyWorker{
+            jobs: Mutex::new(HashMap::new()),
+        }
+    }
+
+    fn submit(&self, command: String) -> Result<Uuid, &str> {
+        let id = Uuid::new_v4();
+        let mut guard = self.jobs.lock().unwrap();
+
+        guard.insert(id, command.clone());
+        log::info!("Created Uuid {} for command {}", id, command);
+
+        Ok(id)
+    }
+
+    fn status(&self, uuid: Uuid) -> Result<String, &str> {
+        let guard = self.jobs.lock().unwrap();
+
+        let command = guard.get(&uuid);
+        match command {
+            Some(s) => Ok(s.to_string()),
+            None => Err("Job not found")
+        }
+    }
+
+    fn kill(&self, uuid: Uuid) -> Result<(), &str> {
+        let guard = self.jobs.lock().unwrap();
+
+        let command = guard.get(&uuid);
+        match command {
+            Some(_) => Ok(()),
+            None => Err("Job not found")
+        }
+    }
 }
