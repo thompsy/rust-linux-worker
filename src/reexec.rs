84822754207a0490c4883f1e9c36af1e50b6ce7a
Andrew Thompson <thompsy@downthewire.co.uk>
2025-03-11 21:16:07 +0000

    Canonicalize path

--- src/reexec.rs
+++ src/reexec.rs
@@ -1,3 +1,5 @@
+use std::fs;
+
 use hostname;
 use nix::mount::MsFlags;
 use unshare::{Command, Namespace, Stdio};
@@ -7,7 +9,21 @@ pub fn get_child(command: String) -> Result<unshare::Child, unshare::Error> {
 
     let parts = command.split_ascii_whitespace().collect::<Vec<&str>>();
 
-    // this has to be unsafe because of the call to pre_exec
+    log::info!("command parts {}", parts.len());
+
+    // TODO I need to deal with errors here more sensibly! I need a higher level error type that
+    // incorporates unshare and whatever custom stuff I might want to return here
+    let alpine_fs = "./alpine-fs/";
+    let alpine_fs_result = fs::canonicalize(alpine_fs);
+    match alpine_fs_result {
+        Ok(_) => {
+            log::info!("fs_ok");
+        }
+        Err(_) => log::error!("fs_err"),
+    }
+    let fs = alpine_fs_result.unwrap();
+
+    // NOTE: this has to be unsafe because of the call to pre_exec
     unsafe {
         // TODO extract the rootfs to a newly created tmpDir so it's fresh everytime
         Command::new(parts[0])
@@ -38,8 +54,8 @@ pub fn get_child(command: String) -> Result<unshare::Child, unshare::Error> {
                 Namespace::User,
             ])
             // NOTE: this should use pivot_root and a tmp directory
-            .chroot_dir("/tmp/alpine/")
-            // Idetally I'd like to let the caller spawn, but I'm having trouble returning the &mut
+            .chroot_dir(fs)
+            // Ideally I'd like to let the caller spawn, but I'm having trouble returning the &mut
             // Command.
             .spawn()
     }
